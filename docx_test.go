7ffd8fc88e6a44d6a2eea413ddf3329165eb966b
xavier@DellXPSA <xavier@DellXPSA>
2025-08-29 12:48:21 +0200

    more tests & debugs

--- docx_test.go
+++ docx_test.go
@@ -1,9 +1,12 @@
 package mydocx
 
 import (
+	"archive/zip"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -129,4 +132,70 @@ func TestDocModifyTpl4(t *testing.T) {
 		fmt.Println("Error:", err)
 		t.Fatal(err)
 	}
+}
+
+func TestExtractXMLFiles(t *testing.T) {
+	t.Log(t.Name(), "is extracting XML files and folders from test.docx")
+
+	docxPath := source
+	extractDir := filepath.Join("testFiles", "test-extracted")
+
+	err := extractDocxXML(docxPath, extractDir)
+	if err != nil {
+		fmt.Println("Error:", err)
+		t.Fatal(err)
+	}
+
+	t.Logf("Successfully extracted XML files to: %s", extractDir)
+}
+
+func extractDocxXML(docxPath, extractDir string) error {
+	r, err := zip.OpenReader(docxPath)
+	if err != nil {
+		return fmt.Errorf("failed to open docx file: %w", err)
+	}
+	defer r.Close()
+
+	err = os.MkdirAll(extractDir, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create extract directory: %w", err)
+	}
+
+	for _, f := range r.File {
+		if strings.HasSuffix(f.Name, ".xml") || f.FileInfo().IsDir() {
+			err := extractFile(f, extractDir)
+			if err != nil {
+				return fmt.Errorf("failed to extract %s: %w", f.Name, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func extractFile(f *zip.File, destDir string) error {
+	filePath := filepath.Join(destDir, f.Name)
+
+	if f.FileInfo().IsDir() {
+		return os.MkdirAll(filePath, f.FileInfo().Mode())
+	}
+
+	if err := os.MkdirAll(filepath.Dir(filePath), 0755); err != nil {
+		return err
+	}
+
+	rc, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	outFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.FileInfo().Mode())
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, rc)
+	return err
 }
